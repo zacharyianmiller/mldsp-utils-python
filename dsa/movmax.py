7b579643553376a31c013b48fb2311c00007a1b7
Zachary Miller <zacharyianmiller1@gmail.com>
2025-09-14 21:41:29 -0500

    movmax update

--- dsa/movmax.py
+++ dsa/movmax.py
@@ -9,6 +9,8 @@ __date__ = "2025-09-06"
 
 import sys
 
+from matplotlib import ticker
+
 """ movmax.py: 
 
 """
@@ -45,26 +47,59 @@ class MovMax:
         return np.fmax(self.front_max, self.back.pop())
 
 
-def movmax_driver():
-    mov_max = MovMax(kernel_size=3)
+def movmax_driver(selection: str):
 
-    # Simple example
-    # x = [0, 1, 2, 3, 2, 1, 0, 0, 0]
-    # y: list = [0] * len(x)
-    # for n in range(len(x)):
-    #     y[n] = mov_max.process(x[n])
+    # Basic example
+    if selection == "1":
+        mov_max = MovMax(kernel_size=5)
+
+        x = np.random.rand(100)
+        y: list = [0] * len(x)
+        for n in range(len(x)):
+            y[n] = mov_max.process(x[n])
+
+        ax, fig = plt.subplots()
+
+        # Format x-axis
+        plt.plot(x)
+        plt.xlabel("Samples")
+        plt.plot(y, color='r', linestyle='--')
+
+        # Format y-axis
+        plt.ylabel('Normalized value [unipolar]')
+        plt.title('Moving Maximum Filter', fontweight='bold')
+        plt.show()
 
     # Audio example
-    x, fs = sf.read("../testing/audio/SnareTop.wav")
-    x = np.abs(x[20000:22500,1])
-    y = np.zeros(x.shape[0])
-    for n in range(x.shape[0]):
-        y[n] = mov_max.process(x[n])
+    elif selection == "2":
+        mov_max = MovMax(kernel_size=4096)
 
+        x, fs = sf.read("../testing/audio/SnareTop.wav")
+        x = np.abs(x[0:int(len(x)/2)-4000,1]) / np.max(np.abs(x)) # mono, normalized
+        y: list = [0] * x.shape[0]
+        for n in range(x.shape[0]):
+            y[n] = mov_max.process(x[n])
 
-    plt.plot(x)
-    plt.plot(y, color='r', linestyle='--')
-    plt.show()
+        # Format x-axis
+        plt.plot(x)
+        plt.xlabel("Samples")
+        ax = plt.gca()
+        ticks = ax.get_xticks().tolist()
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(['0' if x == 0 else '{:}k'.format(int(0.001 * x)) for x in ticks])
+
+        # Format y-axis
+        plt.plot(y, color='r', linestyle='--')
+        plt.ylim([0.2, 1.025])
+        plt.ylabel('Normalized value [unipolar]')
+        plt.title('Moving Maximum Filter', fontweight='bold')
+        plt.show()
+
+    else:
+        print("Invalid selection.")
 
 if __name__ == "__main__":
-    movmax_driver()
+    print("Choose example type to display:")
+    print("1. Basic example w/ random values")
+    print("2. Audio")
+    movmax_driver(input("Selection: "))
